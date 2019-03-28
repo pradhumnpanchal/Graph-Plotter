ededa6726ba8a2a7161b0e0d2b295b52a1f9c2f1
pradhumnpanchal <pradhumn44@gmail.com>
2019-03-28 20:44:22 +0530

    beginnig 2

--- source/src/GraphPlot.java
+++ source/src/GraphPlot.java
@@ -6,26 +6,32 @@ import java.sql.Statement;
 import java.util.*;
 import java.awt.*;
 public class GraphPlot extends JPanel{
-    static int x = 0 , y = 0;
+    static int x = 40 , y = 350;
     public void paintComponent(Graphics g) {
-        g.setColor(Color.BLUE);
+
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/try","root","root");
             Statement stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery("SELECT * FROM graph");
+            int x1 = 20;
             while(rs.next()) {
-                System.out.print(rs.getInt(1));
+                //System.out.print(rs.getInt(1));
+                g.setColor(Color.BLUE);
+                g.drawLine(x1,y,x,rs.getInt(1));
+                x1 = x;
+                x += 20;
+                y = rs.getInt(1);
             }
             con.close();
         }catch(Exception e){ System.out.println(e);}
-        g.drawLine();
+
     }
 
     public static void main(String[] args) {
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("Draw Line");
+        //JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Graph Plotter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setBackground(Color.white);
